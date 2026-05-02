43549c50ba8ed3f814ff62fd89b83621b4e2b784
Saleem-bazhil <ab588538@gmail.com>
2026-05-02 16:42:22 +0530

    u

--- src/controller/opencall.controller.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const opencall.controller = () => {
-  return (
-    <div>opencall.controller</div>
-  )
-}
-
-export default opencall.controller
--- src/models/opencall.models.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const opencall.models = () => {
-  return (
-    <div>opencall.models</div>
-  )
-}
-
-export default opencall.models
